29da06a8c831923a821a04a81771102469b5dc92
Jonathan Morgan <jbmorgan@ncsu.edu>
2013-02-18 23:52:51 -0500

    Initial Commit

---
+++ Save Us Ada/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  Save Us Ada
+//
+//  Created by Jon Morgan on 2/18/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ Save Us Ada/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Save Us Ada
+//
+//  Created by Jon Morgan on 2/18/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
